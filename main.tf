4c7d3696906eae7d9942e8388cca6e7dcacc30cb
Benjamin Bennett <bennett.d.ben@gmail.com>
2023-05-21 20:45:44 +0100

    Add public subnet

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
 module "vpc" {
   source = "git::https://github.com/bendbennett/aws-vpc"
 
-  cidr_block = var.cidr_block
-  region = var.region
+  region = var.vpc_region
+
+  cidr_block = var.vpc_cidr_block
+}
+
+module "subnet-public" {
+  source = "git::https://github.com/bendbennett/aws-subnet"
+
+  region = var.subnet_region
+
+  availability_zones = var.subnet_availability_zones
+  cidr_blocks = var.subnet_cidr_blocks_public
+  vpc_id = module.vpc.vpc_id
 }
