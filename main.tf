e9fbc2b3b0e9d597211ab0e9183dc70767df8ce3
Benjamin Bennett <bennett.d.ben@gmail.com>
2023-05-26 22:49:41 +0100

    Update vars

--- main.tf
+++ main.tf
@@ -1,7 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
 module "vpc" {
   source = "git::https://github.com/bendbennett/aws-vpc"
-
-  vpc_region = var.vpc_region
 
   vpc_cidr_block = var.vpc_cidr_block
 }
@@ -9,9 +20,7 @@ module "vpc" {
 module "subnet-public" {
   source = "git::https://github.com/bendbennett/aws-subnet"
 
-  region = var.vpc_region
-
-  availability_zones = var.subnet_availability_zones
+  availability_zones = var.availability_zones
   cidr_blocks = var.subnet_cidr_blocks_public
   vpc_id = module.vpc.vpc_id
 }
