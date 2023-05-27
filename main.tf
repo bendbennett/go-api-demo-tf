6558961d84bb2c4919efbebe9725a435cfff8bf2
Benjamin Bennett <bennett.d.ben@gmail.com>
2023-05-27 10:44:37 +0100

    Add private subnet

--- main.tf
+++ main.tf
@@ -9,5 +9,17 @@ module "subnet-public" {
 
   availability_zones = var.availability_zones
   cidr_blocks = var.subnet_cidr_blocks_public
+  internet_gateway_id = module.vpc.internet_gateway_id
+  public_subnet = true
+  vpc_id = module.vpc.vpc_id
+}
+
+module "subnet-private" {
+  source = "git::https://github.com/bendbennett/aws-subnet"
+
+  availability_zones = var.availability_zones
+  cidr_blocks = var.subnet_cidr_blocks_private
+  nat_gateway_ids = module.subnet-public.nat_gateway_ids
+  public_subnet = false
   vpc_id = module.vpc.vpc_id
 }
