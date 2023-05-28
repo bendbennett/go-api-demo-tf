62fd5607c79b025a5aa458197c470d937b797bed
Benjamin Bennett <bennett.d.ben@gmail.com>
2023-05-28 17:37:33 +0100

    Add security group and load balancer

--- main.tf
+++ main.tf
@@ -23,3 +23,30 @@ module "subnet-private" {
   public_subnet = false
   vpc_id = module.vpc.vpc_id
 }
+
+module "security-group-load-balancer" {
+  source = "git::https://github.com/bendbennett/aws-security-group"
+
+  security_group_rules_cidr_blocks = var.security_group_rules_cidr_blocks_load_balancer_web
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_elb" "load_balancer" {
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 443
+    lb_protocol = "https"
+    ssl_certificate_id = var.ssl_certificate_id
+  }
+  cross_zone_load_balancing = var.load_balancer_cross_zone_load_balancing
+  name = var.load_balancer_name
+  security_groups = [module.security-group-load-balancer.security_group_id]
+  subnets = module.subnet-public.subnet_ids
+}
--- vars.tf
+++ vars.tf
@@ -1,6 +1,14 @@
 variable "availability_zones" {}
 
+variable "load_balancer_cross_zone_load_balancing" {}
+
+variable "load_balancer_name" {}
+
 variable "region" {}
+
+variable "security_group_rules_cidr_blocks_load_balancer_web" {}
+
+variable "ssl_certificate_id" {}
 
 variable "subnet_cidr_blocks_private" {}
 
