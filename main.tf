dd6abb71352ba27143c9d494969953592d3e3119
Benjamin Bennett <bennett.d.ben@gmail.com>
2023-05-28 17:59:37 +0100

    Modifying load balancer

--- main.tf
+++ main.tf
@@ -46,4 +46,7 @@ resource "aws_elb" "load_balancer" {
     ssl_certificate_id = var.ssl_certificate_id
   }
   cross_zone_load_balancing = var.load_balancer_cross_zone_load_balancing
+  name = var.load_balancer_name
+  security_groups = [module.security-group-load-balancer.security_group_id]
+  subnets = module.subnet-public.subnet_ids
 }
