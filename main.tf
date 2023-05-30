694e291d222511034ee3a119f5b939e94eaad353
Benjamin Bennett <bennett.d.ben@gmail.com>
2023-05-30 20:25:08 +0100

    Remove name from ALB target group

--- main.tf
+++ main.tf
@@ -32,7 +32,6 @@ module "security-group-load-balancer" {
 }
 
 resource "aws_lb_target_group" "target_group" {
-  name = "load_balancer_target_group"
   port = 80
   protocol = "HTTP"
   vpc_id = module.vpc.vpc_id
